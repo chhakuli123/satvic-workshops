3567289d5e4fcf16a3c1f74b80f37e236f3d9f7c
Chhakuli <chhakulizingare12@gmail.com>
2024-07-20 18:19:54 +0530

    feat: add workshop card for desktop and mobile screens

---
+++ src/components/home/section/workshop/MobileWorkshopCard.tsx
@@ -0,0 +1,118 @@
+import Image from 'next/image';
+import { MdOutlineCurrencyRupee } from 'react-icons/md';
+import { TiStarFullOutline } from 'react-icons/ti';
+
+import {
+  Button,
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from '@/components/ui';
+
+import { Workshop } from './workshopTypes';
+
+const MobileWorkshopCard: React.FC<{ workshop: Workshop }> = ({ workshop }) => (
+  <div className="visible overflow-hidden rounded-xl bg-white shadow-md lg:hidden">
+    <div className="relative">
+      <Carousel>
+        <CarouselContent>
+          {[workshop.image, workshop.image, workshop.image].map(
+            (src, index) => (
+              <CarouselItem key={index}>
+                <Image
+                  src={src}
+                  alt={`${workshop.title} - Image ${index + 1}`}
+                  width={400}
+                  height={200}
+                  className="h-full w-full object-cover xl:h-48"
+                />
+              </CarouselItem>
+            )
+          )}
+        </CarouselContent>
+        <CarouselPrevious className="absolute left-2 top-1/2 -translate-y-1/2" />
+        <CarouselNext className="absolute right-2 top-1/2 -translate-y-1/2" />
+      </Carousel>
+      <div className="absolute left-2 top-2 rounded-md bg-white px-2 py-1 text-xs">
+        <p className="font-normal">Starts in:</p>
+        <p className="font-semibold">{workshop.startsIn}</p>
+      </div>
+    </div>
+
+    <div className="p-4 pb-1">
+      <h3 className="text-[22px] font-bold text-gray-800">{workshop.title}</h3>
+
+      <div className="my-2 flex items-center gap-1.5">
+        <span className="flex items-center gap-1 rounded-md bg-success-50 px-1 py-0.5 text-sm text-white">
+          {workshop.rating} <TiStarFullOutline />
+        </span>
+        <span className="text-sm font-semibold text-gray-600">
+          {workshop.reviews} reviews
+        </span>
+      </div>
+
+      <div className="my-4 flex items-center justify-between rounded-lg border border-base-100 px-3 py-1.5 text-xs shadow-[0_4px_20px_rgba(43,43,43,0.08)]">
+        <div className="flex items-center gap-2">
+          <Image
+            src="/assets/icons/calender.svg"
+            alt="Calender"
+            width={18}
+            height={18}
+            className="h-[18px] w-[18px]"
+          />
+          <span className="text-sm font-medium text-base-800">
+            {workshop.startDate}
+          </span>
+        </div>
+        <div className="w-6 rotate-90 border bg-[#F3F3F3] opacity-50"></div>
+
+        <div className="flex items-center gap-2">
+          <Image
+            src="/assets/icons/game-icons_duration.svg"
+            alt="Calender"
+            width={18}
+            height={18}
+            className="h-[18px] w-[18px]"
+          />
+          <span className="text-sm font-medium text-base-800">
+            {workshop.duration}
+          </span>
+        </div>
+        <div className="w-6 rotate-90 border bg-[#F3F3F3] opacity-50"></div>
+
+        <div className="flex items-center gap-2">
+          <Image
+            src="/assets/icons/globe_language.svg"
+            alt="Calender"
+            width={18}
+            height={18}
+            className="h-[18px] w-[18px]"
+          />
+          <span className="text-sm font-medium text-base-800">
+            {workshop.language}
+          </span>
+        </div>
+      </div>
+
+      <p className="mb-3 text-sm text-gray-600">{workshop.description}</p>
+    </div>
+
+    <div className="flex items-center justify-between p-4 shadow-[0_-4px_20px_rgba(43,43,43,0.06)]">
+      <Button variant="ghost" className="">
+        View details
+      </Button>
+      <Button variant="default" className="h-10 px-4 py-2">
+        Join challenge
+        <div className="w-6 rotate-90 border bg-[#F3F3F3] opacity-50"></div>
+        <span className="flex items-center">
+          <MdOutlineCurrencyRupee size={18} />
+          {workshop.price}{' '}
+        </span>
+      </Button>
+    </div>
+  </div>
+);
+
+export default MobileWorkshopCard;
---
+++ src/components/home/section/workshop/WorkshopsCard.tsx
@@ -0,0 +1,159 @@
+import Image from 'next/image';
+import { GoHeartFill } from 'react-icons/go';
+import { LuUsers } from 'react-icons/lu';
+import { MdOutlineCurrencyRupee } from 'react-icons/md';
+import { TiStarFullOutline } from 'react-icons/ti';
+
+import {
+  Button,
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from '@/components/ui';
+
+import { Workshop } from './workshopTypes';
+
+const WorkshopCard: React.FC<{ workshop: Workshop }> = ({ workshop }) => (
+  <div
+    className={`hidden bg-white lg:grid  ${
+      workshop.id === 1 ? 'rounded-t-3xl' : 'rounded-3xl'
+    } grid-cols-2 gap-[30px] overflow-hidden p-6 pt-10 shadow-[0_4px_20px_rgba(43,43,43,0.08)]`}
+  >
+    <div className="relative h-full w-full">
+      <Carousel>
+        <CarouselContent
+          className={`${workshop.id === 2 ? 'h-[22.8rem]' : 'h-[20.8rem]'} ${
+            workshop.id === 3 && 'h-[24.4rem]'
+          } `}
+        >
+          {[workshop.image, workshop.image, workshop.image].map(
+            (src, index) => (
+              <CarouselItem key={index}>
+                <Image
+                  src={src}
+                  alt={`${workshop.title} - Image ${index + 1}`}
+                  width={400}
+                  height={300}
+                  className="h-full w-full rounded-xl object-cover"
+                />
+              </CarouselItem>
+            )
+          )}
+        </CarouselContent>
+        <CarouselPrevious className="absolute left-4 top-1/2 -translate-y-1/2" />
+        <CarouselNext className="absolute right-4 top-1/2 -translate-y-1/2" />
+      </Carousel>
+      {workshop.startsIn !== '' && (
+        <div className="absolute left-4 top-4 flex flex-col rounded-[4.8px] border border-primary-100 bg-white px-[9px] py-1.5 text-base-950">
+          <span className="text-[10px] font-normal leading-[10px]">
+            Starts in:
+          </span>
+          <span className="text-xs font-semibold">{workshop.startsIn}</span>
+        </div>
+      )}
+    </div>
+    <div className=" flex w-full flex-col gap-4">
+      <div className="flex items-start justify-between">
+        <div className="flex flex-col">
+          <h3 className="text-2xl font-semibold text-base-800">
+            {workshop.title}
+          </h3>
+
+          <div className="mt-2 flex items-center gap-1.5">
+            <span className="flex items-center gap-1 rounded-md bg-success-50 px-1 py-0.5 text-sm text-white">
+              {workshop.rating} <TiStarFullOutline />
+            </span>
+            <span className="text-sm font-bold text-base-300">
+              {workshop.reviews} reviews
+            </span>
+          </div>
+        </div>
+
+        {workshop.tag !== '' && (
+          <span className="flex items-center gap-2 rounded-xl bg-primary-300/10 p-1.5 text-sm font-bold text-primary-500">
+            <GoHeartFill size={15} />
+            {workshop.tag}
+          </span>
+        )}
+      </div>
+
+      <p className="text-sm font-medium text-base-800">
+        {workshop.description}
+      </p>
+
+      <div className="flex gap-4">
+        <div className="flex items-center gap-[14px] rounded-xl border border-base-100 px-4 py-5 text-xs shadow-[0_4px_20px_rgba(43,43,43,0.08)]">
+          <Image
+            src="/assets/icons/calender.svg"
+            alt="Calender"
+            width={18}
+            height={18}
+            className="h-[32px] w-[32px]"
+          />
+          <div className="flex flex-col items-start">
+            <span className="text-sm font-medium text-base-800">
+              {workshop.startDate}
+            </span>
+            <span className="text-xs font-medium text-base-300">
+              Start Date
+            </span>
+          </div>
+        </div>
+        <div className="flex items-center gap-[14px] rounded-xl border border-base-100 px-4 py-5 text-xs shadow-[0_4px_20px_rgba(43,43,43,0.08)]">
+          <Image
+            src="/assets/icons/game-icons_duration.svg"
+            alt="Calender"
+            width={18}
+            height={18}
+            className="h-[32px] w-[32px]"
+          />
+          <div className="flex flex-col items-start">
+            <span className="text-sm font-medium text-base-800">
+              {workshop.duration}
+            </span>
+            <span className="text-xs font-medium text-base-300">Duration</span>
+          </div>
+        </div>
+        <div className="flex items-center gap-[14px] rounded-xl border border-base-100 px-4 py-5 text-xs shadow-[0_4px_20px_rgba(43,43,43,0.08)]">
+          <Image
+            src="/assets/icons/globe_language.svg"
+            alt="Calender"
+            width={18}
+            height={18}
+            className="h-[32px] w-[32px]"
+          />
+          <div className="flex flex-col items-start">
+            <span className="text-sm font-medium text-base-800">
+              {workshop.language}
+            </span>
+            <span className="text-xs font-medium text-base-300">Language</span>
+          </div>
+        </div>
+      </div>
+
+      <div>
+        <div className="mb-2">
+          <span className="flex items-center text-xl font-bold text-gray-800">
+            <MdOutlineCurrencyRupee /> {workshop.price}
+          </span>
+        </div>
+
+        <div className="flex items-center gap-4">
+          <Button className="px-8 py-2.5 text-[18px]">
+            Join the challenge
+          </Button>
+          <Button variant="outline" className="text-[18px]">
+            View details
+          </Button>
+        </div>
+        <div className="mt-2 flex items-center gap-2 text-[16px] font-medium text-primary-500">
+          <LuUsers />
+          {workshop.joinedCount} have already joined
+        </div>
+      </div>
+    </div>
+  </div>
+);
+export default WorkshopCard;
