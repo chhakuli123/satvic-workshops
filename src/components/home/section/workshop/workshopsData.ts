1294042b53f667c0396e50c9aa1fffac06b242b4
Chhakuli <chhakulizingare12@gmail.com>
2024-07-20 18:01:57 +0530

    feat: add dummy arry of workshop data

---
+++ src/components/home/section/workshop/workshopsData.ts
@@ -0,0 +1,68 @@
+import { Workshop } from './workshopTypes';
+
+export const workshopsData: Workshop[] = [
+  {
+    id: 1,
+    title: 'Ultimate Health Challenge',
+    image: '/assets/images/workshop-images/image_1.png',
+    rating: 4.8,
+    reviews: 3034,
+    description:
+      'If you want to reach your ultimate peak health, then this workshop is for you. Learn 7 revolutionary habits over a span of 21 days!',
+    startDate: '6th May',
+    duration: '21 Days',
+    language: 'English',
+    price: 990,
+    joinedCount: 3480,
+    tag: 'Must Join',
+    startsIn: '2 DAYS',
+  },
+  {
+    id: 2,
+    title: 'Heal Yourself Challenge',
+    image: '/assets/images/workshop-images/image_2.png',
+    rating: 4.8,
+    reviews: 3034,
+    description:
+      '7 days program to help you learn 9 steps to heal your chronic lifestyle diseases following the Satvic Healing Plan.',
+    startDate: '6th May',
+    duration: '21 Days',
+    language: 'English',
+    price: 990,
+    joinedCount: 3480,
+    tag: 'Highly Recommended',
+    startsIn: '2 DAYS',
+  },
+  {
+    id: 3,
+    title: 'Yoga Challenge',
+    image: '/assets/images/workshop-images/image_3.png',
+    rating: 4.8,
+    reviews: 3034,
+    description:
+      'Learn how to manage stress, anxiety, emotional imbalance and fear. Learn yoga philosophy, asanas and meditations to take charge of your joy!',
+    startDate: '6th May',
+    duration: '21 Days',
+    language: 'Hindi, English',
+    price: 990,
+    joinedCount: 3480,
+    tag: '',
+    startsIn: '2 DAYS',
+  },
+  {
+    id: 4,
+    title: '7-Day Satvic Food Challenge',
+    image: '/assets/images/workshop-images/image_4.png',
+    rating: 4.8,
+    reviews: 3034,
+    description:
+      'Cook-along all three meals with us, and experience the magic of Satvic eating!',
+    startDate: '6th May',
+    duration: '21 Days',
+    language: 'English',
+    price: 990,
+    joinedCount: 3480,
+    tag: '',
+    startsIn: '',
+  },
+];
