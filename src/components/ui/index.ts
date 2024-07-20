dce19d063af2c477b1412a8228e6896dfb3db97b
Chhakuli <chhakulizingare12@gmail.com>
2024-07-20 08:35:01 +0530

    feet: add sheet and popover reusable components

---
+++ src/components/ui/index.ts
@@ -0,0 +1,18 @@
+export {
+  Popover,
+  PopoverTrigger,
+  PopoverContent,
+  PopoverAnchor,
+} from './Popover';
+export {
+  Sheet,
+  SheetPortal,
+  SheetOverlay,
+  SheetTrigger,
+  SheetClose,
+  SheetContent,
+  SheetHeader,
+  SheetFooter,
+  SheetTitle,
+  SheetDescription,
+} from './Sheet';
